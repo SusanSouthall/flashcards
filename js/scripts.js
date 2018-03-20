f06657985449ca126a41b14ef247ec3352dbcd3f
Mike Chu and Susan Southall <mike@>
2018-03-20 11:46:22 -0700

    Add flashcard flipping function.

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+  $("#card1").click(function() {
+    $("#card1 .front").toggle();
+    $("#card1 .back").toggle();
+  });
+  $("#card2").click(function() {
+    $("#card2 .front").toggle();
+    $("#card2 .back").toggle();
+  });
+  $("#card3").click(function() {
+    $("#card3 .front").toggle();
+    $("#card3 .back").toggle();
+  });
+});
