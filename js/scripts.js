aeffc72d10d7ed7c6a4cf1befdebc20fe25cca9f
Mike Chu and Susan Southall <susan@>
2018-03-20 16:03:21 -0700

    Redesign layout.

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,52 @@
 $(document).ready(function() {
   $("#card1").click(function() {
-    $("#card1 .front").toggle();
-    $("#card1 .back").toggle();
+    $(".back").hide();
+    $("#card1back").toggle();
   });
+
   $("#card2").click(function() {
-    $("#card2 .front").toggle();
-    $("#card2 .back").toggle();
+    $(".back").hide();
+    $("#card2back").toggle();
   });
+
   $("#card3").click(function() {
-    $("#card3 .front").toggle();
-    $("#card3 .back").toggle();
+    $(".back").hide();
+    $("#card3back").toggle();
   });
+
+  $("#card4").click(function() {
+    $(".back").hide();
+    $("#card4back").toggle();
+  });
+
+  $("#card5").click(function() {
+    $(".back").hide();
+    $("#card5back").toggle();
+  });
+
+  $("#card6").click(function() {
+    $(".back").hide();
+    $("#card6back").toggle();
+  });
+
+  $("#card7").click(function() {
+    $(".back").hide();
+    $("#card7back").toggle();
+  });
+
+  $("#card8").click(function() {
+    $(".back").hide();
+    $("#card8back").toggle();
+  });
+
+  $("#card9").click(function() {
+    $(".back").hide();
+    $("#card9back").toggle();
+  });
+
+  $("#card10").click(function() {
+    $(".back").hide();
+    $("#card10back").toggle();
+  });
+
 });
